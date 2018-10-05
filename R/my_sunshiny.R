d9f299f09b70a511fdb8c52ce894ce0fe4537a26
Stefano Toffol <stefanotoffol@gmail.com>
2018-10-05 14:09:05 +0200

    Final shiny

--- R/my_sunshiny.R
+++ R/my_sunshiny.R
@@ -12,41 +12,96 @@ library(ggplot2)
 #' @export My_shiny
 #' @exportClass My_shiny
 My_shiny<-
-   setRefClass(
+  setRefClass(
   "My_shiny",
-   fields = list(
-   server_components="list"
-   ),
+  fields = list(
+    server_components="list"
+  ),
   methods=list(
     initialize=function(){
       server_components<<-list()
-      server_components$ui <<- navbarPage("My Application",
-                       tabPanel("Component 1",
-                                # tableOutput("df"),
-                                plotOutput("plot_1")),
-                       tabPanel("Component 2",
-                                textOutput("function_2")),
-                       tabPanel("Component 3",
-                                textOutput("function_3"))
-      )    
+      server_components$ui <<- 
+        navbarPage("My Application",
+           tabPanel("Component 1",
+                    plotOutput("plot_1")),
+           
+           tabPanel("Component 2",
+                    fluidPage(
+                      
+                      titlePanel("Concentration of PM25"),
+                      
+                      fluidRow(
+                        column(2,
+                               radioButtons(inputId = "radio", 
+                                            label = "Select the country you wish to visualize:", 
+                                            choices = c("Turkey", "Italy", "Greece", "Sweden"), 
+                                            inline = FALSE,
+                                            width = NULL)
+                        ),
+                        hr(),
+                        column(10, 
+                               fluidRow(plotlyOutput("plot_2", height = "500px")))))))   
+      
       server_components$server<<- function(input, output){
-        facets = c(
-          "country",
-          "value_pm5"
-        ) 
+        
+        Sys.setenv(
+          'MAPBOX_TOKEN' = 
+            'pk.eyJ1Ijoic3RldG84MjAiLCJhIjoiY2ptYm1hNGoxMDVzODNxcDh5YWYwdWIyeiJ9.vqmnBQELpRxT2klgrWJvuQ')
+        
         countries = list(
           "Turkey",
           "Italy",
           "Greece",
           "Sweden"
         )
+        
         api <- MyShiny::Worldwide_Pollution$new(countries)
         
         output$plot_1 = renderPlot({
+          facets = c(
+            "country",
+            "value_pm5"
+          )
           plot_pm25_means(api$get_facets_all_responses(facets)) 
         })
-        output$function_2 = renderText("This is 2st!")
-        output$function_3 = renderText("This is 3st!")
+        
+        output$plot_2 = renderPlotly({
+          facet_vector<-c(
+            "country",
+            "filename",
+            "value_pm5",
+            "Category PM25",
+            "data_location_latitude",
+            "data_location_longitude")
+          
+          df<-api$get_only_faced_data(api$responses[[input$radio]], facet_vector)
+          if(input$radio=="Italy") zoom <- 4.1
+          else if(input$radio=="Sweden") zoom <- 3
+          else zoom <- 5
+          p<-plot_mapbox(mode = "scattermapbox") %>%
+            add_markers(
+              data = df, y = ~data_location_latitude, x = ~data_location_longitude,
+              color=~as.factor(`Category PM25`), text = ~filename, hoverinfo = "text",
+              hovertext = paste('</br>Category: ', df$`Category PM25`, "</br>Region: ", df$filename,
+                                "</br>Value: ", df$value_pm5),
+              marker=list(size=10), alpha = 0.5,
+              colors = rev(RColorBrewer::brewer.pal(length(unique(df$`Category PM25`)),"PiYG"))) %>%
+            layout(
+              plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
+              mapbox = list(style = 'dark',
+                            scope = "europe",
+                            zoom = zoom,
+                            center = list(lat = mean(as.numeric(df$data_location_latitude)),
+                                          lon = mean(as.numeric(df$data_location_longitude)))),
+              legend = list(orientation = 'h',
+                            font = list(size = 8)),
+              margin = list(l = 0, r = 0,
+                            b = 0, t = 0,
+                            pad = 0)
+            )
+          p
+          
+        })
       }
     },
   run=function(){
